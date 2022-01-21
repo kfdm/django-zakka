98c8a16e08b5026fc721d94b73a3644ccfd81e08
Paul Traylor <kungfudiscomonkey@gmail.com>
2022-01-21 21:27:32 +0900

    Temporary catch for invalid package

--- djangorequests/client.py
+++ djangorequests/client.py
@@ -4,15 +4,19 @@ from django.conf import settings
 from django.contrib.sites.shortcuts import get_current_site
 
 try:
-    from importlib.metadata import version
+    from importlib.metadata import version, PackageNotFoundError
 except ImportError:
-    from importlib_metadata import version
+    from importlib_metadata import version, PackageNotFoundError
 
 DEFAULT_DISTRIBUTION = getattr(settings, "UESR_AGENT_DISTRIBUTION", "django-requests")
 
 
 def user_agent(name):
-    v = version(distribution_name=name)
+    try:
+        v = version(distribution_name=name)
+    except PackageNotFoundError:
+        v = "unknown"
+
     domain = get_current_site(None).domain
     ua = f"{name}/{v} (+{domain})"
     return ua
@@ -40,4 +44,3 @@ def post(url, **kwargs):
 def put(url, **kwargs):
     with DjangoSession() as s:
         return s.put(url, **kwargs)
-
