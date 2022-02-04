c24cdafbba1774527d39a45395627b638a9b1d1c
Paul Traylor <kungfudiscomonkey@gmail.com>
2022-02-04 20:04:54 +0900

    Use @wraps to copy over __doc__ from original api functions

--- djangorequests/client.py
+++ djangorequests/client.py
@@ -2,6 +2,7 @@ import requests
 
 from django.conf import settings
 from django.contrib.sites.shortcuts import get_current_site
+from functools import wraps
 
 try:
     from importlib.metadata import version, PackageNotFoundError
@@ -34,16 +35,38 @@ class DjangoSession(requests.Session):
 USER_AGENT = getattr(settings, "USER_AGENT", user_agent(DEFAULT_DISTRIBUTION))
 
 
-def get(url, **kwargs):
-    with DjangoSession() as s:
-        return s.get(url, **kwargs)
+@wraps(requests.request, assigned=["__doc__"])
+def request(method, url, **kwargs):
+    with DjangoSession() as session:
+        return session.request(method=method, url=url, **kwargs)
 
 
-def post(url, **kwargs):
-    with DjangoSession() as s:
-        return s.post(url, **kwargs)
+@wraps(requests.get, assigned=["__doc__"])
+def get(url, **kwargs) -> requests.Response:
+    return request("get", url, **kwargs)
 
 
-def put(url, **kwargs):
-    with DjangoSession() as s:
-        return s.put(url, **kwargs)
+@wraps(requests.options, assigned=["__doc__"])
+def options(url, **kwargs) -> requests.Response:
+    return request("options", url, **kwargs)
+
+
+@wraps(requests.post, assigned=["__doc__"])
+def post(url, data=None, json=None, **kwargs) -> requests.Response:
+    return request("post", url, data=data, json=json, **kwargs)
+
+
+@wraps(requests.put, assigned=["__doc__"])
+def put(url, data=None, **kwargs) -> requests.Response:
+    return request("put", url, data=data, **kwargs)
+
+
+@wraps(requests.patch, assigned=["__doc__"])
+def patch(url, data=None, **kwargs) -> requests.Response:
+    return request("patch", url, data=data, **kwargs)
+
+
+@wraps(requests.delete, assigned=["__doc__"])
+def delete(url, **kwargs) -> requests.Response:
+    return request("delete", url, **kwargs)
+
