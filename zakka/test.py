f586c35d6510702dbb897c5bd3f8f928dfa41dad
Paul Traylor <kungfudiscomonkey@gmail.com>
2022-06-02 19:25:02 +0900

    Minor refactoring of test_data_base_dir variable

--- zakka/test.py
+++ zakka/test.py
@@ -5,11 +5,14 @@ from django.conf import settings
 
 
 class TestCase(django.test.TestCase):
-    def json_data(self, *path, base_dir=settings.BASE_DIR):
-        path = base_dir.joinpath(*path)
+    test_data_base_dir = settings.BASE_DIR
+
+    def json_data(self, *path):
+        path = self.test_data_base_dir.joinpath(*path)
         with path.open() as fp:
             return json.load(fp)
 
-    def raw_data(self, *path, base_dir=settings.BASE_DIR):
+    def raw_data(self, *path):
+        path = self.test_data_base_dir.joinpath(*path)
         with path.open() as fp:
             return fp.read()
