a56099c74792e6bcb99db52f65bf079ef422eabe
Paul Traylor <kungfudiscomonkey@gmail.com>
2022-08-28 19:12:53 +0900

    Show optional doc string with tasks

--- zakka/contrib/celery/management/commands/tasks.py
+++ zakka/contrib/celery/management/commands/tasks.py
@@ -15,6 +15,14 @@ class Command(LoggingMixin, BaseCommand):
         parser.add_argument("task", nargs="?")
         parser.add_argument("rest", nargs="*")
 
+    def print_task(self, task):
+        name = self.style.MIGRATE_HEADING(task.name.ljust(self.max_length))
+        if task.__doc__:
+            docs = task.__doc__.strip().split("\n")[0]
+        else:
+            docs = ""
+        print(name, docs, sep="\t")
+
     def handle(self, task, eager, rest, **options):
         current_app.loader.import_default_modules()
         if task in current_app.tasks:
@@ -24,5 +32,7 @@ class Command(LoggingMixin, BaseCommand):
                     result = list(result)
                 pprint(result)
         else:
-            for task in sorted(current_app.tasks):
-                print(task)
+            # Get the max length task name for us to use for formatting later
+            self.max_length = max([len(x) for x in current_app.tasks])
+            for name in sorted(current_app.tasks):
+                self.print_task(current_app.tasks[name])
